1ea483ed34158c87867aaddd2822b755942ffc0a
Ondrej Cifka <cifkao@users.noreply.github.com>
2018-12-30 00:59:38 +0100

    Check for output file in model test

--- tests/test_models.sh
+++ tests/test_models.sh
@@ -8,3 +8,4 @@ museflow model RNNGenerator --config mode_rnn_generator.yaml --logdir $dir sampl
 dir=$(mktemp -d)
 museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir $dir train
 museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir $dir run data/modes_A3.pickle $dir/out.pickle 
+test -f $dir/out.pickle
