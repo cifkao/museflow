e48e509fa1f956337dbd9e0e1417cc2460057d3c
Ondrej Cifka <cifkao@users.noreply.github.com>
2018-12-30 10:49:48 +0100

    Better tmp directory handling in model tests

--- tests/test_models.sh
+++ tests/test_models.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -ex
 
-dir=$(mktemp -d)
-museflow model RNNGenerator --config mode_rnn_generator.yaml --logdir $dir train
-museflow model RNNGenerator --config mode_rnn_generator.yaml --logdir $dir sample
+dir="$(mktemp -d)"
+museflow model RNNGenerator --config mode_rnn_generator.yaml --logdir "$dir" train
+museflow model RNNGenerator --config mode_rnn_generator.yaml --logdir "$dir" sample
+rm -rf "$dir"
 
-dir=$(mktemp -d)
-museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir $dir train
-museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir $dir run data/modes_A3.pickle $dir/out.pickle 
-test -f $dir/out.pickle
+dir="$(mktemp -d)"
+museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir "$dir" train
+museflow model RNNSeq2Seq --config mode_transposition.yaml --logdir "$dir" run data/modes_A3.pickle "$dir/out.pickle"
+test -f "$dir/out.pickle"
+rm -rf "$dir"
