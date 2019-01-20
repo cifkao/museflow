81b88839dbf5919d12f9f02ee421ccce04a7e9be
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-01-20 19:00:32 +0100

    Add support for multiple segment lengths to chop_midi

--- museflow/scripts/chop_midi.py
+++ museflow/scripts/chop_midi.py
@@ -15,7 +15,8 @@ def setup_argparser(parser):
     parser.add_argument('input_files', type=argparse.FileType('rb'), nargs='+', metavar='FILE')
     parser.add_argument('output_file', type=argparse.FileType('wb'), metavar='OUTPUTFILE')
     parser.add_argument('-i', '--instrument-re', type=str, default='.*')
-    parser.add_argument('-b', '--bars-per-segment', type=int, default=8)
+    parser.add_argument('-b', '--bars-per-segment', type=lambda l: [int(x) for x in l.split(',')],
+                        default=[8])
     parser.add_argument('-n', '--min-notes-per-segment', type=int, default=2)
     parser.add_argument('-t', '--force-tempo', type=float, default=None)
     parser.add_argument('--skip-bars', type=int, default=0)
@@ -24,6 +25,10 @@ def setup_argparser(parser):
 
 def chop_midi(files, instrument_re, bars_per_segment, min_notes_per_segment=2,
               include_segment_id=False, force_tempo=None, skip_bars=0):
+    if isinstance(bars_per_segment, int):
+        bars_per_segment = [bars_per_segment]
+    bars_per_segment = list(bars_per_segment)
+
     for file in files:
         file_id = file.name
         midi = pretty_midi.PrettyMIDI(file)
@@ -39,31 +44,31 @@ def chop_midi(files, instrument_re, bars_per_segment, min_notes_per_segment=2,
         all_notes = [n for i in instruments for n in i.notes]
 
         downbeats = midi.get_downbeats()[skip_bars:]
-        bps = bars_per_segment
-        for i in range(0, len(downbeats), bps):
-            start = downbeats[i]
-            end = downbeats[i + bps] if i + bps < len(downbeats) else midi.get_end_time()
-            if math.isclose(start, end):
-                continue
+        for bps in bars_per_segment:
+            for i in range(0, len(downbeats), bps):
+                start = downbeats[i]
+                end = downbeats[i + bps] if i + bps < len(downbeats) else midi.get_end_time()
+                if math.isclose(start, end):
+                    continue
 
-            # Find notes that overlap with this segment and clip them.
-            notes = [
-                pretty_midi.Note(
-                    start=max(0., n.start - start),
-                    end=min(n.end, end) - start,
-                    pitch=n.pitch,
-                    velocity=n.velocity)
-                for n in all_notes
-                if n.end >= start and n.start < end
-            ]
+                # Find notes that overlap with this segment and clip them.
+                notes = [
+                    pretty_midi.Note(
+                        start=max(0., n.start - start),
+                        end=min(n.end, end) - start,
+                        pitch=n.pitch,
+                        velocity=n.velocity)
+                    for n in all_notes
+                    if n.end >= start and n.start < end
+                ]
 
-            if len(notes) < min_notes_per_segment:
-                continue
+                if len(notes) < min_notes_per_segment:
+                    continue
 
-            if include_segment_id:
-                yield ((file_id, i, i + bps), notes)
-            else:
-                yield notes
+                if include_segment_id:
+                    yield ((file_id, i, i + bps), notes)
+                else:
+                    yield notes
 
 
 def normalize_tempo(midi, new_tempo=60):
