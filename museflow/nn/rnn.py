f3428f41ff2ef39776454ba81c819bff0761bd9f
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-02-14 16:00:54 +0100

    Add InputWrapper

--- museflow/nn/rnn.py
+++ museflow/nn/rnn.py
@@ -19,3 +19,41 @@ class DropoutWrapper(tf.nn.rnn_cell.DropoutWrapper):  # pylint: disable=abstract
                                       lambda: tf.constant(1.))
 
         super().__init__(cell, **{'dtype': tf.float32, **kwargs})
+
+
+class InputWrapper(tf.nn.rnn_cell.RNNCell):
+    """A wrapper for passing additional input to an RNN cell."""
+
+    def __init__(self, cell, input_fn):
+        """Initialize the wrapper.
+
+        Args:
+            cell: An `RNNCell`.
+            input_fn: A function expecting a scalar tensor argument `batch_size` and returning
+                a tensor of shape `[batch_size, input_size]` to concatenate with the RNN cell input.
+        """
+        super().__init__()
+        self._cell = cell
+        self._dtype = self._cell.dtype
+        self._input_fn = input_fn
+
+    @property
+    def wrapped_cell(self):
+        return self._cell
+
+    @property
+    def state_size(self):
+        return self._cell.state_size
+
+    @property
+    def output_size(self):
+        return self._cell.output_size
+
+    def zero_state(self, batch_size, dtype):
+        with tf.name_scope(type(self).__name__ + "ZeroState", values=[batch_size]):
+            return self._cell.zero_state(batch_size, dtype)
+
+    def __call__(self, inputs, state, scope=None):
+        batch_size = tf.shape(state)[0]
+        inputs = tf.concat([inputs, self._input_fn(batch_size)], axis=-1)
+        return self._cell(inputs, state, scope=scope)
