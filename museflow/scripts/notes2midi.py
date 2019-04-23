17b0b68c8f56f38ddc6d22b7f76a9172211df7c3
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-04-23 18:41:47 +0200

    Add more options to notes2midi

--- museflow/scripts/notes2midi.py
+++ museflow/scripts/notes2midi.py
@@ -1,6 +1,7 @@
 """Turn sequences of notes into MIDI files."""
 
 import argparse
+import collections
 import os
 import pickle
 
@@ -13,10 +14,16 @@ def setup_argparser(parser):
     parser.add_argument('output_dir', type=str, metavar='OUTPUTDIR')
     parser.add_argument('-i', '--instrument', type=str, default='')
     parser.add_argument('--drums', action='store_true')
-    parser.add_argument('-p', '--program', type=int, default=None)
-    parser.add_argument('--stretch', type=str, default=None)
-    parser.add_argument('--tempo', type=float, default=None)
-    parser.add_argument('--time-signature', type=str, default=None)
+    parser.add_argument('-p', '--program', type=int)
+    parser.add_argument('--stretch', type=str)
+    parser.add_argument('--tempo', type=float)
+    parser.add_argument('--time-signature', type=lambda s: tuple(int (x) for x in s.split('/')),
+                        default=(4,4))
+    parser.add_argument('--resolution', type=int, default=480)
+    parser.add_argument('--range', type=lambda r: [None if x == '' else int(x)
+                                                   for x in r.split(':')])
+    parser.add_argument('--group-by-name', action='store_true')
+    parser.add_argument('--time-unit', type=float)
 
 
 def main(args):
@@ -36,14 +43,29 @@ def main(args):
         else:
             args.stretch = float(args.stretch)
             tempo = tempo / args.stretch
-
     if args.tempo:
         tempo = args.tempo
 
     data = pickle.load(args.input_file)
-
     fill_length = len(str(len(data) - 1))
+
+    if args.range:
+        data = data[slice(*args.range)]
+
+    if args.group_by_name:
+        grouped = collections.defaultdict(list)
+        for (name, start, end), notes in data:
+            start, end = start * args.time_unit, end * args.time_unit
+            grouped[name].extend(_shift_and_clip(note, start, end)
+                                 for note in notes
+                                 if note.start + start < end and note.end > 0.)
+        data = list(grouped.items())
+
     for i, segment in enumerate(data):
+        midi = pretty_midi.PrettyMIDI(initial_tempo=tempo, resolution=args.resolution)
+
+        if args.range and args.range[0]:
+            i += args.range[0]
         index = str(i).zfill(fill_length)
         if isinstance(segment, list):
             notes = segment
@@ -61,6 +83,10 @@ def main(args):
                 note.start *= args.stretch
                 note.end *= args.stretch
 
+        # Remove notes with length below the MIDI resolution
+        notes = [note for note in notes
+                 if midi.time_to_tick(note.start) < midi.time_to_tick(note.end)]
+
         # Some notes might be overlapping, we need to split them between multiple tracks.
         # TODO: This calls for a more efficient implementation.
         tracks = [[]]
@@ -74,16 +100,12 @@ def main(args):
                     track.append(note)
                     break
 
-            # Always keep the last track empty
+            # Always keep the last track empty (we delete it afterwards)
             if tracks[-1]:
                 tracks.append([])
         del tracks[-1]
 
-        midi = pretty_midi.PrettyMIDI(initial_tempo=tempo, resolution=480)
-
-        if args.time_signature:
-            numer, denom = args.time_signature.split('/')
-            midi.time_signature_changes[:] = [pretty_midi.TimeSignature(int(numer), int(denom), 0.)]
+        midi.time_signature_changes[:] = [pretty_midi.TimeSignature(*args.time_signature, 0.)]
 
         for track in tracks:
             instrument = pretty_midi.Instrument(name=args.instrument,
@@ -93,3 +115,11 @@ def main(args):
             midi.instruments.append(instrument)
 
         midi.write(os.path.join(args.output_dir, fname))
+
+
+def _shift_and_clip(note, start, end):
+    return pretty_midi.Note(
+        start=start + max(note.start, 0),
+        end=min(start + note.end, end),
+        pitch=note.pitch,
+        velocity=note.velocity)
