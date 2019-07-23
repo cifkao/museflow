2b1eafabf7a0bbcd55fe5588aa333bd5d33c9279
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-07-23 16:07:47 +0200

    Introduce BaseTrainer abstract class

--- museflow/trainer.py
+++ museflow/trainer.py
@@ -1,3 +1,5 @@
+import abc
+import copy
 import os
 import types
 
@@ -8,21 +10,16 @@ from museflow import logger
 from museflow.config import configurable
 
 
-@configurable(['latest_saver', 'best_saver'])
-class BasicTrainer:
-    """A class implementing a basic training/validation loop, model saving and model loading."""
+class BaseTrainer(metaclass=abc.ABCMeta):
+    """A base class for trainers, implementing a training step and model loading and saving."""
 
-    def __init__(self, dataset_manager, logdir, logging_period, validation_period=None,
-                 training_ops=None, session=None, write_summaries=True,
-                 train_dataset_name='train', val_dataset_name='val'):
+    def __init__(self, dataset_manager, logdir, logging_period, training_ops=None,
+                 session=None, write_summaries=True):
         self.session = session or tf.Session()
         self._dataset_manager = dataset_manager
         self._logdir = logdir
-        self._ops = training_ops or BasicTrainer.TrainingOps(loss=None, train_op=())
         self._logging_period = logging_period
-        self._validation_period = validation_period
-        self._train_dataset_name = train_dataset_name
-        self._val_dataset_name = val_dataset_name
+        self._ops = training_ops or BasicTrainer.TrainingOps(loss=None, train_op=())
 
         self._global_step_tensor = tf.train.get_or_create_global_step()
         self._step = 0
@@ -35,75 +32,18 @@ class BasicTrainer:
             self._writer = tf.summary.FileWriter(logdir=self._logdir, graph=tf.get_default_graph())
         else:
             self._writer = None
-        with tf.name_scope('savers'):
-            self._latest_saver = self._cfg['latest_saver'].configure(tf.train.Saver,
-                                                                     name='latest', max_to_keep=2)
-            self._best_saver = self._cfg['best_saver'].configure(tf.train.Saver,
-                                                                 name='best', max_to_keep=1)
 
     @property
     def step(self):
         return self._step
 
-    def train(self):
-        train_generator = self.iter_train(period=None)
-        return next(train_generator)
-
-    def iter_train(self, period=0):
-        """Return a generator that runs the training loop.
-
-        Every `period` training steps, the generator yields an object with the attributes `step`,
-        `train_loss`, `last_val_loss`, `best_val_loss` and `best_val_step`. Note that the first
-        time, `step` will be 0 and the loss values possibly unknown.
-
-        If `period` is not specified, `validation_period` is used instead.
-        """
-        if period == 0:
-            period = self._validation_period
-
-        self.session.run(self._ops.init_op)
-
-        state = types.SimpleNamespace(step=self._step,
-                                      train_loss=None,
-                                      last_val_loss=None,
-                                      best_val_loss=np.inf,
-                                      best_val_step=-1)
-
-        def validate_and_save():
-            state.last_val_loss = self.validate(write_summaries=True)
-            if state.last_val_loss < state.best_val_loss:
-                state.best_val_loss, state.best_val_step = state.last_val_loss, self._step
-                self.save_variables('best')
-            self.save_variables('latest')
-
-        while True:
-            if self._validation_period is not None and self._step % self._validation_period == 0:
-                validate_and_save()
-            if period is not None and self._step % period == 0:
-                yield types.SimpleNamespace(**state.__dict__)
-
-            try:
-                state.train_loss, _ = self.training_step()
-                state.step = self._step
-            except tf.errors.OutOfRangeError:
-                logger.info(f'Training finished after {state.step} steps, loss: {state.train_loss}')
-                break
-
-            if np.isnan(state.train_loss):
-                logger.error('NaN loss, stopping training')
-                break
-
-        if self._validation_period is not None:
-            validate_and_save()
-        yield state
-
-    def training_step(self, feed_dict=None, write_summaries=True, log=True):
+    def training_step(self, dataset_name, feed_dict=None, write_summaries=True, log=True):
         if feed_dict is None:
             feed_dict = {}
 
         _, train_summary, train_loss = self._dataset_manager.run(
             self.session, (self._ops.train_op, self._ops.summary_op, self._ops.loss),
-            self._train_dataset_name, feed_dict={self._ops.training_placeholder: True, **feed_dict})
+            dataset_name, feed_dict={self._ops.training_placeholder: True, **feed_dict})
 
         self._step = self.session.run(self._global_step_tensor)
 
@@ -115,30 +55,18 @@ class BasicTrainer:
 
         return train_loss, train_summary
 
-    def validate(self, write_summaries=False):
-        val_losses = self._dataset_manager.run_over_dataset(self.session, self._ops.loss,
-                                                            self._val_dataset_name)
-        mean_loss = np.mean(val_losses)
-
-        if write_summaries:
-            self.write_scalar_summary('{}/loss'.format(self._val_dataset_name), mean_loss)
-
-        return mean_loss
-
-    def save_variables(self, checkpoint_name='latest'):
-        saver = self._best_saver if checkpoint_name == 'best' else self._latest_saver
-        saver.save(
+    def save_variables(self, checkpoint_name):
+        self._get_saver(checkpoint_name).save(
             self.session,
             os.path.join(self._logdir, checkpoint_name + '.ckpt'),
             latest_filename=checkpoint_name + '_checkpoint',
             global_step=self._step)
 
-    def load_variables(self, checkpoint_name='best', checkpoint_file=None):
-        saver = self._best_saver if checkpoint_name == 'best' else self._latest_saver
+    def load_variables(self, checkpoint_name, checkpoint_file=None):
         if not checkpoint_file:
             checkpoint_file = tf.train.latest_checkpoint(self._logdir,
                                                          checkpoint_name + '_checkpoint')
-        saver.restore(self.session, checkpoint_file)
+        self._get_saver(checkpoint_name).restore(self.session, checkpoint_file)
         logger.info('Variables restored from {}'.format(checkpoint_file))
         self._step = self.session.run(self._global_step_tensor)
 
@@ -148,6 +76,11 @@ class BasicTrainer:
         ])
         if self._writer:
             self._writer.add_summary(summary, step if step is not None else self._step)
+
+    @abc.abstractmethod
+    def _get_saver(self, checkpoint_name):
+        """Returns the saver corresponding to the given checkpoint name."""
+        pass
 
     class TrainingOps:
         """
@@ -168,3 +101,147 @@ class BasicTrainer:
             args = vars()
             for name in ['loss', 'train_op', 'init_op', 'summary_op', 'training_placeholder']:
                 setattr(self, name, args[name])
+
+
+DEFAULT = object()
+
+
+@configurable(['latest_saver', 'best_saver'])
+class BasicTrainer(BaseTrainer):
+    """A trainer implementing a basic training/validation loop."""
+
+    def __init__(self, dataset_manager, logdir, logging_period, validation_period=None,
+                 training_ops=None, session=None, write_summaries=True,
+                 train_dataset_name='train', val_dataset_name='val'):
+        super().__init__(dataset_manager=dataset_manager,
+                         logdir=logdir,
+                         logging_period=logging_period,
+                         training_ops=training_ops,
+                         session=session,
+                         write_summaries=write_summaries)
+
+        self._validation_period = validation_period
+        self._train_dataset_name = train_dataset_name
+        self._val_dataset_name = val_dataset_name
+
+        self._savers = {
+            'latest': self._cfg['latest_saver'].configure(tf.train.Saver,
+                                                          name='latest', max_to_keep=2),
+            'best': self._cfg['best_saver'].configure(tf.train.Saver,
+                                                      name='best', max_to_keep=1)
+        }
+
+    def train(self, dataset_name=None):
+        train_generator = self.iter_train(period=None, dataset_name=dataset_name)
+        return next(train_generator)
+
+    def iter_train(self, dataset_name=None, period=DEFAULT):
+        """Return a generator that runs the training loop.
+
+        Every `period` training steps, the generator yields an object with the attributes `step`,
+        `train_loss`, `last_val_loss`, `best_val_loss` and `best_val_step`. Note that the first
+        time, `step` will be 0 and the loss values possibly unknown.
+
+        If `period` is not specified, `validation_period` is used instead.
+        """
+        if period == DEFAULT:
+            period = self._validation_period
+
+        def training_step_fn():
+            loss, _ = self.training_step(dataset_name=dataset_name or self._train_dataset_name)
+            return self._step, loss
+
+        def validation_fn():
+            self.save_variables('latest')
+            return self.validate(write_summaries=True)
+
+        return training_validation_loop(training_step_fn=training_step_fn,
+                                        validation_fn=validation_fn,
+                                        init_fn=lambda: self.session.run(self._ops.init_op),
+                                        best_val_loss_fn=lambda _: self.save_variables('best'),
+                                        validation_period=self._validation_period,
+                                        yield_period=period,
+                                        initial_step=self._step)
+
+    def validate(self, write_summaries=False):
+        val_losses = self._dataset_manager.run_over_dataset(self.session, self._ops.loss,
+                                                            self._val_dataset_name)
+        mean_loss = np.mean(val_losses)
+
+        if write_summaries:
+            self.write_scalar_summary('{}/loss'.format(self._val_dataset_name), mean_loss)
+
+        return mean_loss
+
+    def save_variables(self, checkpoint_name='latest'):
+        super().save_variables(checkpoint_name=checkpoint_name)
+
+    def load_variables(self, checkpoint_name='best', checkpoint_file=None):
+        super().load_variables(checkpoint_name=checkpoint_name, checkpoint_file=checkpoint_file)
+
+    def _get_saver(self, checkpoint_name):
+        return self._savers[checkpoint_name]
+
+
+def training_validation_loop(training_step_fn, init_fn=None, validation_fn=None,
+                             best_val_loss_fn=None, validation_period=None, yield_period=None,
+                             initial_step=0):
+    """Return a generator that runs a basic training loop.
+
+    Every `period` training steps, the generator yields an object holding information about the
+    state of the training.
+
+    Args:
+        training_step_fn: A function that performs one training step and returns the step number
+            and the training loss value.
+        init_fn: A function to call at the beginning of the training loop.
+        validation_fn: A function that performs validation and returns the validation loss.
+        best_val_loss_fn: A function to call when the best validation loss is achieved; the value
+            of the loss is passed as its only argument.
+        validation_period: How many training steps to perform between running the validation.
+        yield_period: How many training steps to perform between two yields of the generator.
+        initial_step: The initial step value.
+
+    Yields:
+        An object with the attributes `step`, `train_loss`, `last_val_loss`, `best_val_loss` and
+        `best_val_step`.
+    """
+    if (validation_period is None) != (validation_fn is None):
+        raise ValueError("'validation_period' and 'validation_fn' must be either both None "
+                         'or both non-None')
+
+    if init_fn is not None:
+        init_fn()
+
+    state = types.SimpleNamespace(step=initial_step,
+                                  train_loss=None,
+                                  last_val_loss=None,
+                                  best_val_loss=np.inf,
+                                  best_val_step=-1)
+
+    def validate():
+        state.last_val_loss = validation_fn()
+        if state.last_val_loss < state.best_val_loss:
+            state.best_val_loss, state.best_val_step = state.last_val_loss, state.step
+            if best_val_loss_fn:
+                best_val_loss_fn(state.best_val_loss)
+
+    while True:
+        if validation_period is not None and state.step % validation_period == 0:
+            validate()
+        if yield_period is not None and state.step % yield_period == 0:
+            yield copy.copy(state)
+
+        try:
+            state.step, state.train_loss = training_step_fn()
+        except tf.errors.OutOfRangeError:
+            logger.info(f'Training finished after {state.step} steps, loss: {state.train_loss}')
+            break
+
+        if np.isnan(state.train_loss):
+            logger.error('NaN loss, stopping training')
+            break
+
+    if validation_period is not None:
+        validate()
+    yield state
