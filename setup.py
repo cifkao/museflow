68228de63ba9904b8b8048c6f949f538f64881be
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-12-31 13:33:37 +0100

    Add provisional magenta.music dependency

--- setup.py
+++ setup.py
@@ -14,14 +14,20 @@ setuptools.setup(
     },
     python_requires='>=3.6',
     install_requires=[
+        '',
         'cached_property',
         'coloredlogs',
         'numpy',
         'pretty_midi',
         'pyyaml',
+        'magenta.music @ git+https://github.com/cifkao/magenta@magenta.music',
     ],
     extras_require={
-        'gpu': ['tensorflow-gpu<2.0'],
-        'nogpu': ['tensorflow<2.0']
+        'gpu': [
+            'tensorflow-gpu<2.0',
+        ],
+        'nogpu': [
+            'tensorflow<2.0',
+        ]
     }
 )
